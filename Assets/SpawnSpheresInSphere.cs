9d927d0e7f13d2e8723704d89aa07c2420bdd48f
McOmghall <pmgberm@gmail.com>
2016-01-21 14:48:18 +0100

    First

---
+++ Assets/SpawnSpheresInSphere.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSpheresInSphere : MonoBehaviour {
+
+    public int spheres = 10;
+    public float gravity = 9.8f;
+    public GameObject sphereToCopyFrom;
+    private Rigidbody[] spheresCreated;
+
+	// Use this for initialization
+	void Start () {
+        this.spheresCreated = new Rigidbody[spheres];
+
+        for (int i = 0; i < spheres; i++) {
+            Vector3 position = Vector3.Scale(Random.insideUnitSphere, this.transform.lossyScale) + this.transform.position;
+            this.spheresCreated[i] = (Object.Instantiate(sphereToCopyFrom, position, Quaternion.identity) as GameObject).GetComponent<Rigidbody>();
+        }
+        Debug.Log("Allocated " + spheresCreated.Length);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+        foreach(Rigidbody body in this.spheresCreated) {
+            body.AddForce((this.transform.position - body.position) * gravity * Time.smoothDeltaTime, ForceMode.Acceleration);
+            body.AddForce(Vector3.right * (body.position.x > 0 ? -1 : 1) * (gravity / 2));
+            body.AddForce(Vector3.forward * (body.position.z > 0 ? -1 : 1) * (gravity / 2));
+        }
+    }
+}

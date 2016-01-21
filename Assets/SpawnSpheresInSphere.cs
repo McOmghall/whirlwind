7c01764dfd7e73673b674b5edbb9f00aedd1e32f
McOmghall <pmgberm@gmail.com>
2016-01-21 20:20:26 +0100

    added lighting

--- Assets/SpawnSpheresInSphere.cs
+++ Assets/SpawnSpheresInSphere.cs
@@ -13,7 +13,7 @@ public class SpawnSpheresInSphere : MonoBehaviour {
         this.spheresCreated = new Rigidbody[spheres];
 
         for (int i = 0; i < spheres; i++) {
-            Vector3 position = Vector3.Scale(Random.insideUnitSphere, this.transform.lossyScale) + this.transform.position;
+            Vector3 position = Vector3.Scale(Random.insideUnitSphere, this.transform.lossyScale * 2) + this.transform.position;
             this.spheresCreated[i] = (Object.Instantiate(sphereToCopyFrom, position, Quaternion.identity) as GameObject).GetComponent<Rigidbody>();
         }
         Debug.Log("Allocated " + spheresCreated.Length);
@@ -27,9 +27,14 @@ public class SpawnSpheresInSphere : MonoBehaviour {
     void FixedUpdate() {
         foreach(Rigidbody body in this.spheresCreated) {
             //body.AddForce((this.transform.position - body.position) * gravity * Time.smoothDeltaTime, ForceMode.Acceleration);
-            body.AddForce(Vector3.right * (body.position.x > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
-            body.AddForce(Vector3.forward * (body.position.z > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
-            body.AddForce(Vector3.up * (body.position.y > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
+
+            if ((this.transform.position - body.position).magnitude > 0.01 * this.transform.localScale.magnitude) {
+                body.AddForce(Vector3.right * (body.position.x > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
+                body.AddForce(Vector3.forward * (body.position.z > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
+                body.AddForce(Vector3.up * (body.position.y > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
+            } else {
+                body.AddForce((this.transform.position - body.position) * 1000 * - gravity * Time.smoothDeltaTime, ForceMode.Impulse);
+            }
         }
     }
 }

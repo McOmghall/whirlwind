85577c980d479be7e50b3a987b32c189b33237b0
McOmghall <pmgberm@gmail.com>
2016-01-23 22:47:53 +0100

    Finished screensaver project

--- Assets/SpawnSpheresInSphere.cs
+++ Assets/SpawnSpheresInSphere.cs
@@ -6,15 +6,16 @@ public class SpawnSpheresInSphere : MonoBehaviour {
     public int spheres = 10;
     public float gravity = 9.8f;
     public GameObject sphereToCopyFrom;
-    private Rigidbody[] spheresCreated;
+    private GameObject[] spheresCreated;
 
 	// Use this for initialization
 	void Start () {
-        this.spheresCreated = new Rigidbody[spheres];
+        this.spheresCreated = new GameObject[spheres];
 
         for (int i = 0; i < spheres; i++) {
             Vector3 position = Vector3.Scale(Random.insideUnitSphere, this.transform.lossyScale * 2) + this.transform.position;
-            this.spheresCreated[i] = (Object.Instantiate(sphereToCopyFrom, position, Quaternion.identity) as GameObject).GetComponent<Rigidbody>();
+            this.spheresCreated[i] = Object.Instantiate(sphereToCopyFrom, position, Quaternion.identity) as GameObject;
+            this.spheresCreated[i].GetComponent<ParticleSystem>().enableEmission = false;
         }
         Debug.Log("Allocated " + spheresCreated.Length);
     }
@@ -25,16 +26,20 @@ public class SpawnSpheresInSphere : MonoBehaviour {
 	}
 
     void FixedUpdate() {
-        foreach(Rigidbody body in this.spheresCreated) {
+        foreach(GameObject body in this.spheresCreated) {
             //body.AddForce((this.transform.position - body.position) * gravity * Time.smoothDeltaTime, ForceMode.Acceleration);
 
-            if ((this.transform.position - body.position).magnitude > 0.01 * this.transform.localScale.magnitude) {
-                body.AddForce(Vector3.right * (body.position.x > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
-                body.AddForce(Vector3.forward * (body.position.z > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
-                body.AddForce(Vector3.up * (body.position.y > 0 ? -1 : 1) * gravity, ForceMode.Acceleration);
+            if ((this.transform.position - body.transform.position).magnitude > 0.1 * this.transform.localScale.magnitude) {
+                body.GetComponent<Rigidbody>().AddForce((Vector3.right * (body.transform.position.x > 0 ? -1 : 1) + Vector3.up * (body.transform.position.y > 0 ? -1 : 1) + Vector3.forward * (body.transform.position.z > 0 ? -1 : 1)) * gravity, ForceMode.Acceleration);
             } else {
-                body.AddForce((this.transform.position - body.position) * 1000 * - gravity * Time.smoothDeltaTime, ForceMode.Impulse);
+                body.GetComponent<Rigidbody>().AddForce((this.transform.position - body.transform.position) * 10 * - gravity * Time.smoothDeltaTime, ForceMode.Impulse);
+                body.GetComponent<ParticleSystem>().enableEmission = true;
+                body.GetComponent<ParticleSystem>().Play();
+                Invoke("KillParticles", body.GetComponent<ParticleSystem>().duration);
+                Debug.Log("Bursting");
             }
         }
     }
+
+
 }
